a58d724f297d60b4f795517b0f5802b89a192f43
PyroChiliarch <pyrochiliarch@gmail.com>
2019-08-28 22:07:00 +1000

    Client Refactored Client into new Class from main program

---
+++ Consolespaceships/Client.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace Consolespaceships
+{
+    class Client
+    {
+        TcpClient tcpClient;
+        Int32 port;
+        NetworkStream stream;
+
+        public Client (Int32 _port)
+        {
+            tcpClient = null;
+            stream = null;
+            port = _port;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting Client");
+            
+            try
+            {
+                //Try to connect to the host
+                tcpClient = new TcpClient("localhost", port);
+                stream = tcpClient.GetStream();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            Console.WriteLine("Client Started");
+        }
+
+        public void Loop()
+        {
+            
+
+            string message = null;
+            try
+            {
+                //Prepare Message
+                message = Console.ReadLine();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                //Send Message
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Sent: {0}", message);
+
+                //Receive response
+                data = new Byte[256];
+                string responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            
+
+            
+        }
+
+        public void Stop ()
+        {
+            stream.Close();
+            tcpClient.Close();
+        }
+
+    }
+}
--- Consolespaceships/Program.cs
+++ Consolespaceships/Program.cs
@@ -15,37 +15,13 @@ namespace Consolespaceships
         {
             Console.Title = "Terminal Space";
 
-            string message = "Hello World";
-            try
+            Client client = new Client(13000);
+
+            client.Start();
+
+            while (true)
             {
-                Int32 port = 13000;
-                TcpClient client = new TcpClient("localhost", port);
-
-                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-
-                NetworkStream stream = client.GetStream();
-
-                stream.Write(data, 0, data.Length);
-
-                Console.WriteLine("Sent: {0}", message);
-
-                data = new Byte[256];
-                string responseData = String.Empty;
-
-                Int32 bytes = stream.Read(data, 0, data.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                Console.WriteLine("Received: {0}", responseData);
-
-                stream.Close();
-                client.Close();
-            }
-            catch (ArgumentNullException e)
-            {
-                Console.WriteLine("ArgumentNullException: {0}", e);
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
+                client.Loop();
             }
 
             Console.WriteLine("\nPress Enter to continue");
