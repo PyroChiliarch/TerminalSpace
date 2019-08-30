753471097af65b614a9745ccc8ca5d8a89c3c3bf
PyroChiliarch <pyrochiliarch@gmail.com>
2019-08-31 00:24:53 +1000

    Changed how client connection are made and handled. Completely changed everything. Multiple clients may now connect at the same time.

--- Consolespaceships/Program.cs
+++ Consolespaceships/Program.cs
@@ -11,51 +11,19 @@ namespace Consolespaceships
 {
     class Program
     {
+        const string version = "v1.0";
         static void Main(string[] args)
         {
-            string title = "Terminal Space";
+            Console.Title = "Terminal Space : Client : " + version;
 
-            String userCommand = null;
-            String serverResponse = null;
-
-
-            Console.Title = title;
-
-            Client client = new Client(13000);
-            client.Start();
-
-
-            while (true)
-            {
-                //Receive user Command, all commands are changed to upper case
-                userCommand = Console.ReadLine().ToUpper();
-
-                if (userCommand == "QUIT")
-                {
-                    //Disconnect  from the server
-                    serverResponse = client.SendCommand("DISCONNECT");
-                    client.Stop();
-                    break;
-                } else if (userCommand == "WHERE AM I")
-                {
-                    //Find out where we are
-                    serverResponse = client.SendCommand(userCommand);
-                } else
-                {
-                    //Just send the message if we have no matching command
-                    serverResponse = client.SendCommand(userCommand);
-                }
-                
-                //Send Users Command
-                serverResponse = serverResponse.ToUpper();
-
-                Console.WriteLine("Received: {0}", serverResponse);
-            }
-
-            Console.WriteLine("\nPress Enter to quit");
-            Console.Read();
-
-
+            //Connect to server and do nothing
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect("127.0.0.1", 8);
+            //Hang once connected to the server
+            Console.ReadLine();
+            s.Close();
+            s.Dispose();
+            
         }
 
         
---
+++ ConsolespaceshipsServer/Listener.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsolespaceshipsServer
+{
+
+    //The listener class once it has started listening for new connections will continuously 
+    //raise the SocketAccepted Event everytime a new connection is made to the server
+    //This will continue until it is told to stop
+    class Listener
+    {
+        //A temporary socket that is used to store the most recent connection
+        Socket s;
+
+
+        //Is the object actively listening for new connections?
+        public bool Listening
+        {
+            get;
+            private set;
+        }
+
+        //What port are we using for connections?
+        public int Port
+        {
+            get;
+            private set;
+        }
+
+        //Constructor
+        public Listener (int port)
+        {
+            Port = port;
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        //Start listening for connections
+        public void Start ()
+        {
+            //Ignore if we are already listening
+            if (Listening)
+                return;
+
+            //Binds socket to Endpoint with specified server port
+            s.Bind(new IPEndPoint(0, Port));
+
+            //Simply sets the state of the socket
+            s.Listen(0);
+            
+            //Asynchronous Operation
+            //Will begin to listen for new connections
+            //The callback function recalls this method so that a loop is made
+            //This allows the Listener to continue accepting new connections
+            s.BeginAccept(callback_newConnection, null);
+
+            //We are now continuously listening for connections
+            Listening = true;
+        }
+
+        //Stop listening for new connections
+        public void Stop ()
+        {
+            //Ignore if we are already not listening
+            if (!Listening)
+                return;
+
+            //Clean up the socket since it is no longer being used
+            s.Close();
+            s.Dispose();
+
+            //Create a fresh socket for when we want to start listening again
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        //Our socket is activley listening for new connections
+        //This will be called everytime it receives a new connection
+        //The new connection is added to the list
+        void callback_newConnection(IAsyncResult asyncResult)
+        {
+            try
+            {
+                //Get the new connection
+                Socket s = this.s.EndAccept(asyncResult);
+
+                if (SocketAccepted != null)
+                {
+                    //Calls the event
+                    //A new socket has been connected
+                    SocketAccepted(s);
+                }
+
+                //Recall this operation again
+                //Forms a loop to continuously listen for connections
+                this.s.BeginAccept(callback_newConnection, null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public delegate void SocketAcceptedHandler(Socket e);
+        public event SocketAcceptedHandler SocketAccepted;
+
+    }
+}
