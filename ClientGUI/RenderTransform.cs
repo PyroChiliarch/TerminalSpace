7e7d2823cd6e3193634b184db45e2bdedc4d2dda
Nathan Vermey <pyrochiliarch@gmail.com>
2019-09-11 21:43:26 +1000

    Merged ExtendedMath with RenderTransform This was the only place they were used

--- ClientGUI/RenderTransform.cs
+++ ClientGUI/RenderTransform.cs
@@ -48,7 +48,7 @@ namespace ClientGUI
         public void Translate(Vector3 translation)
         {
 
-            position += Helpers.Graphics.ExtendedMath.RotateVecByQuat(translation, rotation);
+            position += RotateVecByQuat(translation, rotation);
 
         }
 
@@ -64,7 +64,7 @@ namespace ClientGUI
             //p = q*vq
             //
 
-            position += Helpers.Graphics.ExtendedMath.RotateVecByQuat(translation, rotation);
+            position += RotateVecByQuat(translation, rotation);
 
         }
 
@@ -82,6 +82,92 @@ namespace ClientGUI
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //=============================================================================
+        //Math Methods
+        //=============================================================================
+
+
+        private static Vector3 RotateVecByQuat(Vector3 vec, Quaternion quat)
+        {
+            //http://math.stackexchange.com/questions/40164/how-do-you-rotate-a-vector-by-a-unit-quaternion
+            //
+            //conjugate is the mirror? of a quaternion, and is a quaternion itself
+            //a pure quaternion is a point in space, also W=0, so we can turn translation to a pure quat
+            //
+            //where p = pure quaternion, q* = quaternion conjugate, v = pure quat based on vector, q = quaternion
+            //p = q*vq
+            //
+
+
+            Quaternion conjugate = quat;
+            conjugate.Conjugate();
+
+            Quaternion pureQuat = new Quaternion(vec, 0f);
+
+            Quaternion newVec = conjugate * pureQuat * quat;
+
+            return new Vector3(newVec.X, newVec.Y, newVec.Z);
+        }
+
+
+
+
+
+
+
+
+
+
+        //Returns Radians
+        private static Vector4 QuatToAxisAngle(Quaternion quat)
+        {
+            //http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToAngle/
+
+
+            quat.Normalize();
+
+            double angle = 2 * Math.Acos(quat.W);
+            double s = Math.Sqrt(1 - (quat.W * quat.W));
+
+            double x;
+            double y;
+            double z;
+
+            if (s < 0.001) //If s is almost 0 we dont need to normalize
+            {
+                x = quat.X;
+                y = quat.Y;
+                z = quat.Z;
+            }
+            else
+            {
+                x = quat.X / s;
+                y = quat.Y / s;
+                z = quat.Z / s;
+            }
+
+
+
+            return new Vector4((float)x, (float)y, (float)z, (float)angle);
+
+        }
 
 
 
